fd52ec380ea2e705f0a5c1bc15f906eaaeccfa23
preethy12 <preethyray12@.com>
2023-07-10 23:11:52 +0530

    update

--- accordian.js
+++ accordian.js
@@ -1,18 +1,32 @@
-const accord=document.querySelectorAll('.accordian__container')
+// const accord=document.querySelectorAll('.accordian__container')
 
-accord.forEach( item => {
-const title =item.querySelector('.accordian__container__title')
+// accord.forEach( item => {
+// const title =item.querySelector('.accordian__container__title')
 
-title.addEventListener('click', ()=> {
-    for( i=0; i<accord.length;i++){
-        if(accord[i]!=item){
-            accord[i].classList.remove('active');
+// title.addEventListener('click', ()=> {
+//     for( i=0; i<accord.length;i++){
+//         if(accord[i]!=item){
+//             accord[i].classList.remove('active');
+//         }
+//         else{
+//         item.classList.toggle('active');
+//         }
+
+//     }
+
+// });
+// })
+var acc = document.getElementsByClassName("accordian__container__title");
+        var i;
+        for (i = 0; i < acc.length; i++) {
+        acc[i].addEventListener("click", function() {
+
+            this.classList.toggle("active");
+            var panel = this.nextElementSibling;
+            if (panel.style.display === "block") {
+            panel.style.display = "none";
+            } else {
+            panel.style.display = "block";
+            }
+        });
         }
-        else{
-        item.classList.toggle('active');
-        }
-
-    }
-
-});
-})
